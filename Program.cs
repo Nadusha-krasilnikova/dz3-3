9af53902497ea040de2873100b55f178645a3674
Nadusha <nadusha230275@gmail.com>
2022-07-07 21:49:55 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// возвести число А в степень В
+Console.WriteLine("введите число которое надо возвести в степень ");
+ int a =int.Parse(Console.ReadLine());
+Console.WriteLine("введите степень ");
+int b=int.Parse(Console.ReadLine());
+double distance;
+distance=Math.Pow(a,b);
+Console.WriteLine($"число в степени = {distance}");
